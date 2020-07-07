311f74f319f0f753061c38f97a07309279e7d035
Mark May <mamark90@gmail.com>
2020-07-07 16:58:50 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,6 +1,6 @@
 class BankAccount
-  attr_accessor :name, :balance
-  def initialize(name, balance = 1000)
+  attr_accessor :name, :balance, :open
+  def initialize(name, balance = 1000, status = "open")
     @name = name
     @balance = balance
   end
